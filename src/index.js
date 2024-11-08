e714743915945000ea798af2653f67d28cdc83fb
Lasmeratzi <ralfmatthew12@gmail.com>
2024-11-08 15:14:25 +0800

    cm

--- src/index.js
+++ src/index.js
@@ -3,17 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Pages/Login/Login'; 
-import Shtm from './Pages/Shtm/Shtm';  // Admin home
-import Sbit from './Pages/Sbit/Sbit';
-import Sslate from './Pages/Sslate/Sslate';
-import Sarfaid from './Pages/Sarfaid/Sarfaid';
+import Departments from './Pages/Departments/Departments';
+import Clubs from './Pages/Clubs/Clubs';
 import Shome from './Pages/Shome/Shome';  // User home
 import About1 from './Pages/About1/About1'; // About page
-import Aboutm from './Pages/Aboutm/Aboutm'; // About page
+import Events from './Pages/Events/Events'; // About page
 import reportWebVitals from './reportWebVitals';
-import Sbitclubs from './Pages/Sbitclubs/Sbitclubs';
-import Sarfaidclubs from './Pages/Sarfaidclubs/Sarfaidclubs';
-import Shtmclubs from './Pages/Shtmclubs/Shtmclubs';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -22,17 +17,10 @@ root.render(
       <Routes>
         <Route path="/" element={<Login />} />  {/* Login page as the default route */}
         <Route path="/user" element={<Shome />} />  {/* Regular user home page */}
-        <Route path="/shtm" element={<Shtm />} />  {/* Admin home page */}
-        <Route path="/sbit" element={<Sbit />} />
-        <Route path="/sslate" element={<Sslate />} />
-        <Route path="/sarfaid" element={<Sarfaid />} />
+        <Route path="/departments" element={<Departments />} />
         <Route path="/about" element={<About1 />} />  {/* About page */}
-        <Route path="/aboutm" element={<Aboutm />} />
-        
-         {/* clubs page */}
-        <Route path="/Sbitclubs" element={<Sbitclubs />} />
-        <Route path="/Sarfaidclubs" element={<Sarfaidclubs />} />
-        <Route path="/Shtmclubs" element={<Shtmclubs />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/clubs" element={<Clubs />} />
       </Routes>
     </Router>
   </React.StrictMode>
